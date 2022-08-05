fafb359e6700ecad318a5f2664d2db40bdd39ffe
tinho1123 <tinhoefaela@gmail.com>
2022-08-05 18:47:06 -0300

    feat: requistion to backend on front-end

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,5 +3,4 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: `http://${process.env.REACT_APP_HOSTNAME}:3001/api`,
 })
-
 export default api;
--- src/contexts/ContextDataProvider.js
+++ src/contexts/ContextDataProvider.js
@@ -1,11 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Context from './Context';
+import api from '../api/axiosConfig'
 
 function ContextDataProvider({ children }) {
     const [createEmployeePopUp, setCreateEmployeePopUp] = useState(false);
     const [editEmployeePopUp, setEditEmployeePopUp] = useState(false);
     const [deleteEmployeePopUp, setDeleteEmployeePopUp] = useState(false);
 
+    const [employee, setEmployee] = useState()
+    
+    useEffect(() => {
+        getData()
+    }, [setEmployee])
+
+    const getData = async () => {
+        await api.get('/employee', )
+        .then(({ data }) => setEmployee(data))
+        .catch((err) => console.log(err))
+    }
+
+    console.log(employee);
     const value = {
         createEmployeePopUp,
         setCreateEmployeePopUp,
@@ -13,6 +27,8 @@ function ContextDataProvider({ children }) {
         setEditEmployeePopUp,
         deleteEmployeePopUp,
         setDeleteEmployeePopUp,
+        employee,
+        setEmployee
     }
 
     return (
