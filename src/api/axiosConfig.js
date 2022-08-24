73ec4808546f9a084b67bffebe663caa03bc9696
tinho1123 <tinhoefaela@gmail.com>
2022-08-24 08:45:37 -0300

    axios error

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `http://${process.env.REACT_APP_HOSTNAME}/api`,
+    baseURL: `${process.env.REACT_APP_HOSTNAME}/api`,
 })
 export default api;
